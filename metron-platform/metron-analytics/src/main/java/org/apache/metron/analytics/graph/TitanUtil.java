82df25ac8dd11aebc0d7801c2720a145af0af0bf
cstella <cestella@gmail.com>
2016-07-20 10:12:27 -0400

    commit.

--- metron-platform/metron-analytics/src/main/java/org/apache/metron/analytics/graph/TitanUtil.java
+++ metron-platform/metron-analytics/src/main/java/org/apache/metron/analytics/graph/TitanUtil.java
@@ -1,9 +1,36 @@
 package org.apache.metron.analytics.graph;
 
+import com.thinkaurelius.titan.core.TitanFactory;
+import com.thinkaurelius.titan.core.TitanGraph;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.hbase.HBaseConfiguration;
+import org.apache.hadoop.hbase.HConstants;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
 public enum TitanUtil {
   INSTANCE;
-  private class TitanConnection {
+  static Map<String, TitanGraph> connectionPool = new HashMap<>();
 
+  public TitanGraph getGraph(String graph) {
+    synchronized (connectionPool) {
+      TitanGraph ret = connectionPool.get(graph);
+      if(ret == null) {
+        ret = createConnection(graph);
+        connectionPool.put(graph, ret);
+      }
+      return ret;
+    }
   }
 
+  private static TitanGraph createConnection(String graph) {
+    Configuration config = HBaseConfiguration.create();
+    TitanGraph ret = TitanFactory.build().set("storage.backend", "hbase")
+                                         .set("storage.hostname", config.get(HConstants.ZOOKEEPER_QUORUM))
+                                         .set("tablename", graph)
+                                         .open();
+    return ret;
+  }
 }
