b71409a32ce8df618da9de5da7b1d5c0b74eb80d
cstella <cestella@gmail.com>
2016-08-24 09:42:01 -0400

    update

--- transitive_dependency_analyzer/list_dependencies.sh
+++ transitive_dependency_analyzer/list_dependencies.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-mvn dependency:list | grep "^\[INFO\]   " | awk '{print $2}' | sort | uniq 
+mvn dependency:list | grep "^\[INFO\]   " | awk '{print $2}' | grep -v "org.apache" | grep -v "test" | sort | uniq 
