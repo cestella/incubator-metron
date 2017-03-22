9a191d4517e5c03f91308c904728a72f439d9fce
cstella <cestella@gmail.com>
2017-03-22 03:15:40 -0400

    Updating spout.

--- metron-platform/metron-storm-kafka/src/main/java/org/apache/metron/storm/kafka/flux/StormKafkaSpout.java
+++ metron-platform/metron-storm-kafka/src/main/java/org/apache/metron/storm/kafka/flux/StormKafkaSpout.java
@@ -18,15 +18,28 @@
 
 package org.apache.metron.storm.kafka.flux;
 
+import org.apache.kafka.common.errors.WakeupException;
+import org.apache.log4j.Logger;
 import org.apache.storm.kafka.spout.KafkaSpout;
 import org.apache.storm.kafka.spout.KafkaSpoutConfig;
 
 public class StormKafkaSpout<K, V> extends KafkaSpout<K, V> {
+  private static final Logger LOG = Logger.getLogger(StormKafkaSpout.class);
   protected KafkaSpoutConfig<K,V> _spoutConfig;
   protected String _topic;
   public StormKafkaSpout(SimpleStormKafkaBuilder<K,V> builder) {
     super(builder.build());
     this._topic = builder.getTopic();
     this._spoutConfig = builder.build();
+  }
+
+  @Override
+  public void close() {
+    try {
+      super.close();
+    }
+    catch(WakeupException we) {
+      LOG.error(we.getMessage(), we);
+    }
   }
 }
