4f5394e3731dbbf8051f51684be0fad21e840033
cstella <cestella@gmail.com>
2017-04-20 21:12:07 -0400

    Getting tests working and the packet_filter working.

--- metron-platform/metron-pcap/src/main/java/org/apache/metron/pcap/pattern/ByteArrayMatchingUtil.java
+++ metron-platform/metron-pcap/src/main/java/org/apache/metron/pcap/pattern/ByteArrayMatchingUtil.java
@@ -1,4 +1,21 @@
-package org.apache.metron.pattern;
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.metron.pcap.pattern;
 
 import com.google.common.cache.CacheBuilder;
 import com.google.common.cache.CacheLoader;
@@ -32,8 +49,15 @@ public enum ByteArrayMatchingUtil {
   }
 
   public boolean match(String pattern, byte[] data) throws ExecutionException {
-
+    if(pattern == null) {
+      return false;
+    }
     Searcher<SequenceMatcher> searcher = sequenceMatchers.get(pattern);
-    return !searcher.searchForwards(data).isEmpty();
+    if(data == null) {
+      return false;
+    }
+    else {
+      return !searcher.searchForwards(data).isEmpty();
+    }
   }
 }
